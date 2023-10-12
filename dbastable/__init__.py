fe1f2f247de7bfc52435bc74fecddfff50cce389
Julio Campagnolo <juliocampagnolo@gmail.com>
2023-10-12 15:27:09 -0300

    infrastructure reorganization

--- dbastable/__init__.py
+++ dbastable/__init__.py
@@ -1,8 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Manage SQL databases in a simplier way."""
 
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
+
 from ._sqldb import SQLDatabase, SQLTable, SQLRow, SQLColumn
 from . import where
+from .tests import run_tests
 
 __all__ = ['SQLDatabase', 'SQLTable', 'SQLRow', 'SQLColumn',
-           'where']
+           'where', 'run_tests', '__version__']
---
+++ dbastable/tests/__init__.py
@@ -0,0 +1,5 @@
+
+def run_tests():
+    """Run unittests for dbastable."""
+    from . import test_db
+    test_db.run_tests()
